bf148382b1afd19796b8644e83d5e3847dbadc92
Felipe Fernandes Klajn <afkfelipe30@gmail.com>
2025-08-06 17:03:20 -0300

    Add files via upload

---
+++ 1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num_1=0;
+    int num_2=0;
+    int result=0;
+    
+    scanf("%d",&num_1);
+    scanf("%d",&num_2);
+    
+    result=num_1+num_2;
+    
+    printf("SOMA = %d\n",result);
+    return 0;
+}
