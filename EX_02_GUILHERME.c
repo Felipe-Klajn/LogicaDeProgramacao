34505e2e5126d2e9bbc25eb444734e95a30bc37e
Felipe Fernandes Klajn <afkfelipe30@gmail.com>
2025-10-09 17:25:35 -0300

    Update EX_02_GUILHERME.c

--- EX_02_GUILHERME.c
+++ EX_02_GUILHERME.c
@@ -8,6 +8,11 @@ Nome VARCHAR(60),
 DtNasc DATE
 );
 
+CREATE TABLE Modelo(
+codMod INT NOT NULL PRIMARY KEY,
+Desc_2 VARCHAR(40)
+);
+
 CREATE TABLE Veiculo(
 Placa VARCHAR (8) NOT NULL PRIMARY KEY,
 Modelo_codMod INT,
@@ -15,11 +20,6 @@ Cliente_CPF INT,
 cor VARCHAR(20),
 FOREIGN KEY (Modelo_codMod) REFERENCES Modelo(codMod),
 FOREIGN KEY (Cliente_cpf) REFERENCES Cliente (cpf)
-);
-
-CREATE TABLE Modelo(
-codMod INT NOT NULL PRIMARY KEY,
-Desc_2 VARCHAR(40)
 );
 
 CREATE TABLE Patio(
@@ -39,3 +39,11 @@ hsSaida VARCHAR(10),
 FOREIGN KEY (Patio_num) REFERENCES Patio(num),
 FOREIGN KEY (Veiculo_placa) REFERENCES Veiculo(placa)
 );
+
+INSERT INTO Cliente(CPF, Nome, DtNasc) values("04551750944", "Guilherme", "2000-01-14"),
+("04551740947", "Jose", "1999-01-13"),
+("04551550940", "Alberto", "1998-03-25");
+
+INSERT INTO Modelo(codMod, Desc_2) values ("20", "Ford Ka"),
+("25", "Hyundai HB20");
+
